834e5f935b168b7c32065ebb9210376a9351be37
Kang0 <kevinkangg@gmail.com>
2019-03-28 14:01:55 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@post = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+@cool = @post.categories.build(name: "Cool")
+@post.save
+
+@category = Category.create(name: "Cool")
+@post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+@user = User.create(username: 'Samantha')
+
+@category = Category.create(name: "Cool")
+@post = Post.create(title: "Awesome", content: "Wow!")
+@post.categories << @category
+@user = User.create(username: 'Kevin')
+@comment1 = Comment.create(content: "This was great!", user: @user, post: @post)
+
+@post = Post.create(title: "Feeling", content: "It")
+@user = User.create(username: 'CoolGuy5')
+@comment = @post.comments.build(content: "Great Post!", user: @user)
+@post.save
